da93cc455d24feae1ad911bfbfbb3d7e7c702965
Kashin Nikita <kashin.nik008@gmail.com>
2022-02-13 20:31:09 +0300

    a

--- first.py
+++ first.py
@@ -36,86 +36,91 @@ def barycentric_in(coor, triangle):
     pointXY = [pointX, pointY]
     return pointXY
 
-#project = QgsProject.instance()
-#layer_name = "protected_areas"
-#list_layers = project.mapLayersByName(layer_name)
-#my_layer = list_layers[0]
+
+project = QgsProject.instance()
+
+layer_name = "treangle"
+list_layers = project.mapLayersByName(layer_name)
+if list_layers:
+    treangle_layer = list_layers[0]
+    project.removeMapLayer(treangle_layer.id())
+
+layer_name = "points200"
+list_layers = project.mapLayersByName(layer_name)
+points200_layer = list_layers[0]
+
 point_array = []
-my_layer = iface.activeLayer()
-features = my_layer.getFeatures()
+features = points200_layer.getFeatures()
 for feature in features:
     geom = feature.geometry()
     list_points = geom.asMultiPoint()
     pointXY = [list_points[0].x(), list_points[0].y()]
     point_array.append(pointXY)
-    #print ("Point: x: ", list_points[0].x(),"y: ", list_points[0].y())
-    #print ("Next object:")
-    #for point in list_points[0]:
-        #print("Point x: ", point)
-        #print("Point: x: ", point.x(),"y: ", point.y(), end='\n')
-my_layer.removeSelection()
 
 points = np.array(point_array)
 tri = Delaunay(points)
 triangleXY = points[tri.simplices]
 
-## create layer
-#vl = QgsVectorLayer("MultiPolygon", "treangle", "memory")
-#pr = vl.dataProvider()
-#
-## add a feature
-#fet = QgsFeature()
-#for triangl in triangleXY:
-#    fet.setGeometry(QgsGeometry.fromPolygonXY([[QgsPointXY(triangl[0][0], triangl[0][1]), QgsPointXY(triangl[1][0], triangl[1][1]), QgsPointXY(triangl[2][0], triangl[2][1]), QgsPointXY(triangl[0][0], triangl[0][1])]]))
-#    pr.addFeatures([fet])
-#
-## update layer's extent when new features have been added
-## because change of extent in provider is not propagated to the layer
-#vl.updateExtents()
-#
+# create layer
+suri = "MultiPolygon?crs=epsg:20008&index=yes"
+vl = QgsVectorLayer(suri, "treangle", "memory")
+pr = vl.dataProvider()
+vl.updateExtents()
+
+# add a feature
+fet = QgsFeature()
+for triangl in triangleXY:
+    fet.setGeometry(QgsGeometry.fromPolygonXY([[QgsPointXY(triangl[0][0], triangl[0][1]), QgsPointXY(triangl[1][0], triangl[1][1]), QgsPointXY(triangl[2][0], triangl[2][1]), QgsPointXY(triangl[0][0], triangl[0][1])]]))
+    pr.addFeatures([fet])
+    vl.updateExtents()
+
+vl.updateExtents()
+if not vl.isValid():
+    print("Layer failed to load!")
+else:
+    QgsProject.instance().addMapLayer(vl)
 #print("fields:", len(pr.fields()))
 #print("features:", pr.featureCount())
 #e = vl.extent()
 #print("extent:", e.xMinimum(), e.yMinimum(), e.xMaximum(), e.yMaximum())
-#
 ## iterate over features
 #features = vl.getFeatures()
 #for fet in features:
-#    print("F:", fet.id(), fet.attributes(), fet.geometry().asPolygon())
+#    print("F:", fet.geometry().asPolygon(), end = '\n')
 
 
-crs = QgsProject.instance().crs()
-transform_context = QgsProject.instance().transformContext()
-save_options = QgsVectorFileWriter.SaveVectorOptions()
-save_options.driverName = "ESRI Shapefile"
-save_options.fileEncoding = "windows-1251"
-
-fields = QgsFields()
-writer = QgsVectorFileWriter.create(
-"E:\Никита\ИС-118\Диплом\VKR\\triangle.shp",
-fields,
-QgsWkbTypes.MultiPolygon,
-crs,
-transform_context,
-save_options
-)
-
-if writer.hasError() != QgsVectorFileWriter.NoError:
-    print("Error when creating shapefile: ",  writer.errorMessage())
-
-fet = QgsFeature()
-for triangl in triangleXY:
-    fet.setGeometry(QgsGeometry.fromPolygonXY([[QgsPointXY(triangl[0][0], triangl[0][1]), QgsPointXY(triangl[1][0], triangl[1][1]), QgsPointXY(triangl[2][0], triangl[2][1]), QgsPointXY(triangl[0][0], triangl[0][1])]]))
-    writer.addFeature(fet)
-
-del writer
-
-path = "E:\Никита\ИС-118\Диплом\VKR\\triangle.shp"
-vlayer = QgsVectorLayer(path, "triangle", "ogr")
-if not vlayer.isValid():
-    print("Layer failed to load!")
-else:
-    QgsProject.instance().addMapLayer(vlayer)
+#crs = QgsProject.instance().crs()
+#transform_context = QgsProject.instance().transformContext()
+#save_options = QgsVectorFileWriter.SaveVectorOptions()
+#save_options.driverName = "ESRI Shapefile"
+#save_options.fileEncoding = "windows-1251"
+#
+#fields = QgsFields()
+#writer = QgsVectorFileWriter.create(
+#"E:\Никита\ИС-118\Диплом\VKR\\triangle.shp",
+#fields,
+#QgsWkbTypes.MultiPolygon,
+#crs,
+#transform_context,
+#save_options
+#)
+#
+#if writer.hasError() != QgsVectorFileWriter.NoError:
+#    print("Error when creating shapefile: ",  writer.errorMessage())
+#
+#fet = QgsFeature()
+#for triangl in triangleXY:
+#    fet.setGeometry(QgsGeometry.fromPolygonXY([[QgsPointXY(triangl[0][0], triangl[0][1]), QgsPointXY(triangl[1][0], triangl[1][1]), QgsPointXY(triangl[2][0], triangl[2][1]), QgsPointXY(triangl[0][0], triangl[0][1])]]))
+#    writer.addFeature(fet)
+#
+#del writer
+#
+#path = "E:\Никита\ИС-118\Диплом\VKR\\triangle.shp"
+#vlayer = QgsVectorLayer(path, "triangle", "ogr")
+#if not vlayer.isValid():
+#    print("Layer failed to load!")
+#else:
+#    QgsProject.instance().addMapLayer(vlayer)
 
 #areaOfInterest = QgsRectangle(450290,400520, 450750,400780)
 #QgsTriangle(QgsPoint,QgsPoint,QgsPoint)
